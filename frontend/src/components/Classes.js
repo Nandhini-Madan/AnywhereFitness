175b838f73b8b987a1414a160980d849391ca2d6
unknown <nandhinimadan20@gmail.com>
2021-08-09 16:39:50 -0700

    Error in Private Class

--- frontend/src/components/Classes.js
+++ frontend/src/components/Classes.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+
 const Classes=()=>{
+    const defaultClientState={
+        first_name:"",
+        username:"",
+        role:""
+    }
     const[allclass,setAllClass]=useState([])
+    const [clientData, SetClientData]=useState(defaultClientState)
+   
     
     useEffect(()=>{
-        axios.get('')
+        axios.get('http://localhost:5000/api/client/classes')
         .then(res=>{
-            console.log(res,"classes")
+            console.log(res.data,"classes")
             setAllClass(res.data)
+            SetClientData(res.jwt)
         })
         .catch(
             err=>{
@@ -19,7 +28,9 @@ const Classes=()=>{
     return(
         
         <>
-      
+      <h1> Welcome :{clientData.first_name}</h1>
+     
+      <p>{allclass}</p>
         </>
     )
 }
