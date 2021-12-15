84f87a4f827881c5e9eca0f84245c00053a5cb77
unknown <nandhinimadan20@gmail.com>
2021-12-15 10:48:00 -0800

    Error while accessing value from .env file

--- frontend/src/components/ClientSessions.js
+++ frontend/src/components/ClientSessions.js
@@ -2,40 +2,37 @@ import React, { useEffect, useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { Row, Col, Card, Button } from "react-bootstrap";
 import fitness from "../asserts/fitness1.jpg"
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
+const stripePublishKey = process.env.REACT_APP_PUBLISHABLE_KEY
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
 const Clientsessions = () => {
-   /*
-    const defaultState = {
-        name: "",
-        type: "",
-        start_time: "",
-        duration: "",
-        intensity: "",
-        location: "",
-        max_class: ""
-    }*/
-    let history=useHistory()
+    console.log("key1", stripePublishKey)
+
+
+    let history = useHistory()
     const [mySessions, setMySessions] = useState([])
-    const getSessions=()=>{
+    const getSessions = () => {
         axiosWithAuth().get("https://anywherefitness21.herokuapp.com/api/client/classes/sessions")
-        .then(res => {
-            console.log(res.data)
-            setMySessions(res.data.classes)
-        })
-        .catch(err => {
-            console.log("Errror displaying", err)
-        })
+            .then(res => {
+                console.log(res.data)
+                setMySessions(res.data.classes)
+            })
+            .catch(err => {
+                console.log("Errror displaying", err)
+            })
     }
 
     useEffect(() => {
-        getSessions()}
-        , [])
+        getSessions()
+    }, [])
 
     const deleteclass = (id) => {
         console.log("delete", id)
-        const sessionID=id
+        const sessionID = id
         axiosWithAuth().delete(`https://anywherefitness21.herokuapp.com/api/client/classes/sessions/${sessionID}`)
             .then(res => {
                 console.log("Successfully deleted", res)
@@ -45,25 +42,20 @@ const Clientsessions = () => {
                 console.log("Error", err)
             })
     }
-    /*
-    const EditSession = (id) => {
-        console.log("edit",id)
-        history.push(`/editSessions/${id}`)
-    }*/
-    const backToClass=()=>{
+
+    const backToClass = () => {
         history.push("/Client")
     }
 
     return (
+
         <>
             <h1>My sessions</h1>
             <br></br>
             <Row xs={1} md={2} className="g-4">
                 {mySessions.map(((mySessions) => (
                     <Col key={mySessions.sessionID} >
-
                         <Card>
-
                             <Card.Img variant="top" src={fitness} />
                             <Card.Body>
                                 <Card.Title>Name:{mySessions.name}</Card.Title>
@@ -76,15 +68,17 @@ const Clientsessions = () => {
                                     Start Time: {mySessions.start_time}
                                 </Card.Text>
                                 <Button variant="primary" className="danger" onClick={deleteclass.bind(this, mySessions.sessionID)}> Delete</Button>
-                                <Button onClick={backToClass}>Back</Button>  
-                              </Card.Body>
+                                <Button onClick={backToClass}>Back</Button>
+                            </Card.Body>
                         </Card>
                     </Col>
                 )))}
             </Row>
             <button>Checkout</button>
+       </>
 
-        </>
+
+
     )
 
 }
